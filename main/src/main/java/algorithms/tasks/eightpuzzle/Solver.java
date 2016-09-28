14d0adebb3f85f5c71cb6524f39fa9490370532e
Yur Ovchynnikov <hawk.toma@gmail.com>
2016-09-28 19:49:05 +0300

    Added description of 8-puzzle task

--- main/src/main/java/algorithms/tasks/eightpuzzle/Solver.java
+++ main/src/main/java/algorithms/tasks/eightpuzzle/Solver.java
@@ -7,6 +7,38 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Write a program to solve the 8-puzzle problem
+ * (and its natural generalizations) using the A* search algorithm.
+ * <p>
+ * The 8-puzzle problem is a puzzle invented and popularized by Noyes Palmer Chapman in the 1870s.
+ * It is played on a 3-by-3 grid with 8 square blocks labeled 1 through 8 and a blank square.
+ * Your goal is to rearrange the blocks so that they are in order, using as few moves as possible.
+ * You are permitted to slide blocks horizontally or vertically into the blank square.
+ * <p>
+ * Best-first search.
+ * Now, we describe a solution to the problem that illustrates a general artificial intelligence methodology
+ * known as the A* search algorithm. We define a search node of the game to be a board, the number of moves
+ * made to reach the board, and the previous search node. First, insert the initial search node
+ * (the initial board, 0 moves, and a null previous search node) into a priority queue. Then, delete from
+ * the priority queue the search node with the minimum priority, and insert onto the priority queue all
+ * neighboring search nodes (those that can be reached in one move from the dequeued search node).
+ * Repeat this procedure until the search node dequeued corresponds to a goal board. The success of this
+ * approach hinges on the choice of priority function for a search node. We consider two priority functions:
+ * - Hamming priority function. The number of blocks in the wrong position, plus the number of moves made so
+ * far to get to the search node. Intuitively, a search node with a small number of blocks in the wrong position
+ * is close to the goal, and we prefer a search node that have been reached using a small number of moves.
+ * - Manhattan priority function. The sum of the Manhattan distances (sum of the vertical and horizontal distance)
+ * from the blocks to their goal positions, plus the number of moves made so far to get to the search node.
+ * <p>
+ * Detecting unsolvable puzzles
+ * To detect such situations, use the fact that boards are divided into two equivalence classes with respect
+ * to reachability: (i) those that lead to the goal board and (ii) those that lead to the goal board if we modify
+ * the initial board by swapping any pair of blocks (the blank square is not a block).
+ * To apply the fact, run the A* algorithm on two puzzle instances—one with the initial board and one with
+ * the initial board modified by swapping a pair of blocks—in lockstep (alternating back and forth between
+ * exploring search nodes in each of the two game trees). Exactly one of the two will lead to the goal board.
+ */
 public class Solver {
     private MinPQ<Move> queue = new MinPQ<>();
     private MinPQ<Move> queueTwin = new MinPQ<>();
