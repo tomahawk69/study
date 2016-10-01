73fe9fe1b584fee95b8d488b52da48453f93191d
Yur Ovchynnikov <hawk.toma@gmail.com>
2016-10-01 18:34:42 +0300

    Updated RBT task description

--- main/src/main/java/algorithms/common/datastructure/RedBlackTree.java
+++ main/src/main/java/algorithms/common/datastructure/RedBlackTree.java
@@ -4,7 +4,7 @@ package algorithms.common.datastructure;
  * TODO: Red-black tree
  * Guaranteed logarithmic performance for insert and search
  * O(lg n)
- * Imp[lementations: LLRB
+ * Implementations: Left-leaning red–black
  * rotateLeft, rotateRight
  * flipColors (recolor left and right red nodes)
  */
